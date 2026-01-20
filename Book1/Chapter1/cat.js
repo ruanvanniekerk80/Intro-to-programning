82a1c114275948d637b90aebb7c5eebb2278bdbc
ruanvanniekerk80 <ruan.vanniekerk@live.com>
2026-01-20 13:07:51 +0200

    Add files via upload

---
+++ Book1/Chapter1/cat.js
@@ -0,0 +1,26 @@
+// Define a function called drawCats that takes one parameter
+// howManyTimes determines how many cats will be drawn
+var drawCats = function (howManyTimes) {
+
+   // Loop from 0 up to (but not including) howManyTimes
+   for (var i = 0; i < howManyTimes; i++) {
+
+      // Print the current number and a cat face to the console
+      console.log(i + " =^.^=");
+   }
+};
+
+// Call the drawCats function and draw 10 cats
+drawCats(10);
+
+
+
+// Start a loop with i set to 8; keep looping while i is less than 11; increase i by 1 each time
+for (var i = 8; i < 11; i++) {
+
+   // Print the current value of i followed by a cute face to the console
+   console.log(i + " =^.^=");
+}
+
+// End of the loop
+
---
+++ Book1/Chapter1/forloop.js
@@ -0,0 +1,39 @@
+// Start a loop with i set to 0; run the loop while i is less than or equal to 10;
+// increase i by 2 after each iteration
+for (var i = 0; i <= 10; i += 2) {
+
+   // Print the current value of i to the console
+   console.log(i);
+}
+
+// Start a loop with i set to 1; continue looping while i is less than or equal to 10;
+// increase i by 2 each time to get only odd numbers
+for (var i = 1; i <= 10; i += 2) {
+
+   // Print the current value of i to the console
+   console.log(i);
+}
+
+
+
+// Start a loop with i set to 100
+// Keep looping as long as i is greater than or equal to 0
+// After each loop, subtract 10 from i
+for (var i = 100; i >= 0; i -= 10) {
+
+   // Print the current value of i to the console
+   console.log(i);
+}
+
+// Start a loop with i set to 0; continue looping while i is less than 5;
+// increment i by 1 before the next iteration
+for (var i = 0; i < 5; ++i) {
+
+   // Print the current value of i to the console
+   console.log(i);
+}
+
+
+for (var i = 5; i > 1; --i) {
+   console.log(i);
+}

9801318e5c16c29712fca8001916d46d32daf7fd
ruanvanniekerk80 <ruan.vanniekerk@live.com>
2026-01-21 14:19:54 +0200

    Update tryOut.js

--- Book1/Chapter_2/tryOut(Page 23)/tryOut.js
+++ Book1/Chapter_2/tryOut(Page 23)/tryOut.js
@@ -1,5 +1,11 @@
+// Set the initial number of balloons to 100
 var balloons = 100;
+
+// Multiply the number of balloons by 2
 balloons *= 2;
 
+// Set the initial value of number to 50
 var number = 50;
+
+// Multiply the number by 2
 number *= 2;

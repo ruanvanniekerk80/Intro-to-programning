c197a3efbedbc14531e6f4dd96184c06269a6d06
ruanvanniekerk80 <ruan.vanniekerk@live.com>
2026-01-21 14:18:11 +0200

    Update devide.js

--- Book1/Chapter_2/tryOut(Page 23)/devide.js
+++ Book1/Chapter_2/tryOut(Page 23)/devide.js
@@ -1,5 +1,11 @@
+// Start with 100 balloons
 var balloons = 100;
+
+// Divide the number of balloons by 4
 balloons /= 4;
 
+// Start with 100 as the initial value
 var numbers = 100;
-number /= 2;
+
+// Divide the number by 2
+numbers /= 2;
